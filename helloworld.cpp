6d40fc406021e0164919b136edd5c77f6017d22e
AdvikaGitHub12 <advikabhosle@gmail.com>
2025-07-15 20:43:26 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - E&TC - A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello!"<<endl;
+    cout << "First Program"<<endl;
+    return 0;
+}
+/*
+hello!
+First Program
+
+=== Code Execution Successful ===
+*/
